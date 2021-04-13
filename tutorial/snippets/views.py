4a6df7f9fd9a583e815aa1943032138505b686ca
Katerina964 <katrin.balakina@gmail.com>
2021-04-13 11:57:51 +0300

    fixed bugs

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -3,13 +3,11 @@ from snippets.serializers import SnippetSerializer
 from django.contrib.auth.models import User
 from snippets.serializers import UserSerializer
 from rest_framework import permissions
-from rest_framework import generics
 from snippets.permissions import IsOwnerOrReadOnly
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 from rest_framework import renderers
-from rest_framework.response import Response
 from rest_framework import viewsets
 from rest_framework.decorators import action
 
@@ -41,6 +39,7 @@ class UserViewSet(viewsets.ReadOnlyModelViewSet):
     """
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
 
 @api_view(['GET'])
 def api_root(request, format=None):
